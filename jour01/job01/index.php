c1349fafa41ce79f00ec63151491e2a9ddd3faa9
thomas-spinec <thomas.spinec@laplateforme.io>
2023-01-16 10:53:10 +0100

    création du repo, job01 du jour01 done

---
+++ jour01/job01/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script>
+        alert("Hello Javascript !");
+    </script>
+    <title>Job01-jour01</title>
+</head>
+<body>
+    <h1>Job01-jour01</h1>
+</body>
+</html>
