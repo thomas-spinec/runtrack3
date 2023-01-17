f6dbecff47cbaadc8ca6494214d1714b3a2c25e6
thomas-spinec <thomas.spinec@laplateforme.io>
2023-01-17 10:49:51 +0100

    job02 du jour02 done, affichage/disparition d'un article

---
+++ jour02/job02/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job02-jour02</title>
+    <script src="script.js"></script>
+</head>
+
+<body>
+    <h1>Job02-jour02</h1>
+    <h2>Affichage / suppression d'un élément </h2>
+
+    <button id="button">Show</button>
+</body>
+
+</html>
---
+++ jour02/job02/script.js
@@ -0,0 +1,32 @@
+// attente du chargement du DOM
+document.addEventListener("DOMContentLoaded", function () {
+  // récupération de l'élément
+  const btn = document.querySelector("#button");
+
+  // déclaration de la fonction
+  function showhide() {
+    // vérification de l'état de l'élément
+    if (btn.innerHTML == "Show") {
+      // création de l'élément
+      const article = document.createElement("article");
+      // ajout du texte
+      article.textContent =
+        "L'important n'est pas la chute, mais l'atterissage";
+      // ajout de l'élément
+      document.querySelector("body").appendChild(article);
+      // modification de l'élément
+      btn.innerHTML = "Hide";
+    } else {
+      // suppression de l'élément
+      document.querySelector("article").remove();
+      // modification de l'élément
+      btn.innerHTML = "Show";
+    }
+  }
+
+  // écouteur d'événement
+  btn.addEventListener("click", () => {
+    // affichage de la valeur de l'élément
+    showhide();
+  });
+});
