b3ace6d6473b0bda2c9bad088d4feefed0b25b28
thomas-spinec <thomas.spinec@laplateforme.io>
2023-01-18 12:00:03 +0100

    job01 du jour03 done, affichage et disparition d'un élément html

---
+++ jour03/job01/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job01-jour03</title>
+</head>
+
+<body>
+    <h1>Job01-jour03</h1>
+    <h2>JQuery </h2>
+
+    <button id="show">Show text</button>
+    <button id="hide">Hide text</button>
+    <p>Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit,
+        ensuite on prie.</p>
+
+    <script src="../../jquery-3.6.3.min.js"></script> <!-- mieux de mettre ces liens ici d'après le dernier lien du sujet -->
+    <script src="script.js"></script>
+</body>
+
+</html>
---
+++ jour03/job01/script.js
@@ -0,0 +1,10 @@
+// sécurité du chargement
+jQuery(document).ready(function ($) {
+  // le reste des fonctions
+  $("#show").click(function () {
+    $("p").show();
+  });
+  $("#hide").click(function () {
+    $("p").hide();
+  });
+});
